53f9076c80abac7b90dc1da5e822408d95928683
Amine S <amine.s3y@gmail.com>
2025-06-27 03:45:07 +0200

    fix: imports

--- packages/ui/src/components/sidebar/sidebar.stories.tsx
+++ packages/ui/src/components/sidebar/sidebar.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react-vite";
 import { SidebarProvider } from "./sidebar";
 import { Sidebar } from "./sidebar";
 import { Button } from "../button/button";
--- packages/ui/src/composites/data-table-skeleton/data-table-skeleton.stories.tsx
+++ packages/ui/src/composites/data-table-skeleton/data-table-skeleton.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react-vite";
 
 import { DataTableSkeleton } from "./data-table-skeleton";
 
